2ff6b0387c97fa636cb05647a826ad62a3eb63e1
f0ever0 <thddndud0830@gmail.com>
2023-10-07 22:29:05 +0900

    init : initial setting

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function App() {
+  return (
+    <>
+      <Title>Hello</Title>
+    </>
+  );
+}
+
+export default App;
+
+const Title = styled.h1`
+  color: red;
+`;
